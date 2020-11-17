c5c31af6eab8ba754a69cc0d9d5d4bce00c2799b
Vijay Kumar <vijay.kumar@10xgenomics.com>
2020-11-16 17:01:06 -0800

    Avoid using Makefile, refactor (#9)
    * change dir structure
    
    * finish

---
+++ bwa-sys/build.rs
@@ -0,0 +1,48 @@
+// make -C bwa-sys/bwa/ -n libbwa.a | grep -o -E "[A-Za-z0-9_]+\.c"
+const FILES: &[&str] = &[
+    "bwa/utils.c",
+    "bwa/kthread.c",
+    "bwa/ksw.c",
+    "bwa/bwt.c",
+    "bwa/bntseq.c",
+    "bwa/bwa.c",
+    "bwa/bwamem.c",
+    "bwa/bwamem_pair.c",
+    "bwa/bwamem_extra.c",
+    "bwa/malloc_wrap.c",
+];
+
+// make -C bwa-sys/bwa/ -nd libbwa.a | grep -o -E "[A-Za-z0-9_]+\.h" | sort | uniq
+const HEADERS: &[&str] = &[
+    "bwa/bntseq.h",
+    "bwa/bwa.h",
+    "bwa/bwamem.h",
+    "bwa/bwt.h",
+    "bwa/gpl.h",
+    "bwa/kbtree.h",
+    "bwa/khash.h",
+    "bwa/kseq.h",
+    "bwa/ksort.h",
+    "bwa/kstring.h",
+    "bwa/ksw.h",
+    "bwa/kvec.h",
+    "bwa/malloc_wrap.h",
+    "bwa/utils.h",
+];
+
+fn main() {
+    for file in FILES {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+    for file in HEADERS {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+    cc::Build::new()
+        .define("COMPILATION_TIME_PLACE", "\"build.rs\"")
+        .files(FILES)
+        .flag("-g")
+        .flag("-Wall")
+        .flag("-O2")
+        .flag("-fPIC")
+        .compile("bwa");
+}
